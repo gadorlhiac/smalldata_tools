e875c309a6a819d83267a0018dcf8f57625063ac
Adam <aegger@slac.stanford.edu>
2020-10-02 12:33:09 -0700

    add example for subscriber

--- mpi_scripts/mpi_api.py
+++ mpi_scripts/mpi_api.py
@@ -1,5 +1,6 @@
 import yaml
 import zmq
+import numpy as np
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--cfg_file', help='if specified, has information about what metadata to use', type=str, default='default_config.yml')
@@ -12,6 +13,18 @@ with open(''.join(['mpi_configs/', args.cfg_file])) as f:
 context = zmq.Context()
 socket = context.socket(zmq.PAIR)
 socket.connect(''.join(['tcp://localhost:', str(api_port)]))
+
+# Example for subscribing to np arrays zmq
+#context_data = zmq.Context()
+#socket_data = context_data.socket(zmq.SUB)
+#socket_data.connect(''.join(['tcp://localhost:', '8123']))
+#socket_data.subscribe("")  # All topics
+#while True:
+#    md = socket_data.recv_json(flags=0)
+#    msg = socket.recv(flags=0, copy=False, track=False)
+#    buf = memoryview(msg)
+#    data = np.frombuffer(buf, dtype=md['dtype'])
+#    print('data ', data.reshape(md['shape'])) 
 
 def abort():
     """Abort MPI run"""
